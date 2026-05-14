ebdf3685e87e754276507c0200de31b2a779a78b
KittenLord <vanya.braslavets@gmail.com>
2026-05-14 15:03:15 +0300

    clusterize minesweeper data

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
-
-from collections import deque
+import cv2
 
 
 
@@ -51,6 +50,39 @@ def load_minesweeper_data(path):
     return stats, dayMin, dayMax
 
 def main():
-    print("hello")
+    stats, dayMin, dayMax = load_minesweeper_data("./minesweeper.txt")
+
+    statsRaw = []
+    for s in stats:
+        statsRaw.append((float(s["dayIndex"]), float(s["time"])))
+    statsRaw = np.array(statsRaw, dtype = np.float32)
+
+    mins = statsRaw.min(axis=0)
+    maxs = statsRaw.max(axis=0)
+
+    statsNorm = (statsRaw - mins) / (maxs - mins)
+    # statsNorm = statsRaw
+
+
+
+    k = 7
+    compactness, labels, centers = cv2.kmeans(statsNorm, k, None, (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2), 10, cv2.KMEANS_PP_CENTERS)
+    labels = labels.flatten()
+
+
+
+    plt.figure(figsize=(6, 6))
+
+    for i in range(k):
+        cluster = statsRaw[labels == i]
+        plt.scatter(cluster[:, 0], cluster[:, 1])
+
+    plt.title("minesweeper clusterized")
+    plt.xlabel("day")
+    plt.ylabel("game time")
+    plt.legend()
+    plt.grid(True)
+
+    plt.show()
 
 main()
